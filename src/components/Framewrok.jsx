c55e68ed28f7385cc938b3b53a9b00a81053e825
kevindermot99 <mbonimpayekevin@gmail.com>
2024-07-06 18:10:33 +0200

    framework to a link

--- src/components/Framewrok.jsx
+++ src/components/Framewrok.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Smalltext from "./Smalltext";
 
-function Framewrok({logo, title, desc}) {
+function Framewrok({logo, title, desc, url}) {
   return (
-    <div className="hover:bg-stone-100 cursor-pointer h-fit flex max-sm:flex-col items-start justify-start gap-4 p-5 rounded-2xl">
+    <a href={url} target="_blank" className="hover:bg-stone-100 cursor-pointer h-fit flex max-sm:flex-col items-start justify-start gap-4 p-5 rounded-2xl">
       <div className="h-[60px] shadow-sm p-3 shadow-stone-300 ring-1 ring-stone-200 w-auto aspect-square rounded-full bg-white flex justify-center items-center ">
         <img src={logo} className=" " />
       </div>
@@ -11,7 +11,7 @@ function Framewrok({logo, title, desc}) {
         <h1 className="text-dark-text text-base tracking-tight font-bold ">{title}</h1>
         <p className="text-sm font-normal text-dark-text/80">{desc}</p>
       </div>
-    </div>
+    </a>
   );
 }
 
