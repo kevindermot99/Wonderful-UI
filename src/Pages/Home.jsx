2f2a94ebd8153c1da4b32b503ab68747d1269bc8
kevindermot99 <mbonimpayekevin@gmail.com>
2024-07-19 01:28:58 +0200

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -119,7 +119,7 @@ function Home() {
                 {/* <span className="w-auto h-full flex-1 bg-item-bgl-1 rounded-md "></span> */}
               </div>
               <div className="h-[16px] w-full flex items-center justify-center gap-1">
-                <span className="w-auto h-full aspect-square bg-item-bgl-2 rounded-md "></span>
+                {/* <span className="w-auto h-full aspect-square bg-item-bgl-2 rounded-md "></span> */}
                 <span className="w-auto h-full flex-1 bg-item-bgl-1 rounded-md "></span>
               </div>
             </div>
