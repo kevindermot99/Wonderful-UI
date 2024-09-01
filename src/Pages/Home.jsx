2f792bf7ded7cf95b12fa632dc9359a23f39aa45
kevindermot99 <mbonimpayekevin@gmail.com>
2024-09-01 04:59:00 +0200

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -85,7 +85,7 @@ function Home() {
           >
             <div className="h-[100px] w-[160px] flex items-center justify-center gap-1">
               <div className="h-[8px] w-full bg-item-bgl-1 rounded-md flex items-center justify-center">
-                <span className="w-16 h-full rounded-md bg-item-bgl-2"></span>
+                {/* <span className="w-16 h-full rounded-md bg-item-bgl-2"></span> */}
               </div>
             </div>
             <p className="h-[30px] w-full text-center flex items-start justify-center font-normal tracking-tight text-sm leading-[16px] capitalize ">
